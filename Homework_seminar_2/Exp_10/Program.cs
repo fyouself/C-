35a22748a87566b7e49e76dd5e079c9c554fa17e
Andre <fyouself@gmail.com>
2023-02-02 23:05:18 +0300

    Выполненое Д.З. к семинару № 2

---
+++ Homework_seminar_2/Exp_10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+System.Console.Write("Write number one: ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A < 1000 && A > 99)
+{
+    A = A / 10;
+    System.Console.WriteLine(A % 10);
+}
+
+ else
+{
+    System.Console.WriteLine("Введено не трёхзначное число ");
+}
+
+
+
---
+++ Homework_seminar_2/Exp_15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>7)
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
+else if ( n== 7 || n==6)
+{
+    System.Console.WriteLine("Да");
+}
+else 
+{
+    System.Console.WriteLine("Нет");
+}
