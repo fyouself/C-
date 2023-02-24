7242a878836a3753a3d73b38aab461f57ed174c2
Andre <fyouself@gmail.com>
2023-02-24 17:31:46 +0300

    Выполнено домашнее задание 9

---
+++ Homework_seminar_9/66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int SumElements(int m, int n)
+{
+    if (m == n) return m;
+
+    return m + SumElements(m + 1, n);
+
+}
+
+
+int m = GetNumber("Write M: ");
+int n = GetNumber("Write N: ");
+System.Console.WriteLine(SumElements(m, n));
---
+++ Homework_seminar_9/68/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+
+    if (n == 0) return Akkerman(m - 1, 1);
+
+    if (n > 0 && m > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    return Akkerman( m,  n);
+}
+
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    int n = Convert.ToInt32(Console.ReadLine());
+    return n;
+}
+
+int m = GetNumber("Write M: ");
+int n = GetNumber("Write N: ");
+System.Console.WriteLine(Akkerman(m, n));
